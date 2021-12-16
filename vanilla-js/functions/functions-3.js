0151c8255a7aaef2a781bd6f29e9761b13cb2109
Jamie Garner <jamiegarner123@gmail.com>
2021-12-16 10:55:39 +0000

    add more exercises

---
+++ vanilla-js/functions/functions-3.js
@@ -0,0 +1,17 @@
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk
+// but containing one "needle"
+
+// After your function finds the needle it should return a message
+// (as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+// should return "found the needle at position 5"
+
+const findNeedle = haystack => {
+  
+}
+
+// Call your function below with a different 'haystack' to test it out:
+// findNeedle(<insert your haystack here pls>)
--- vanilla/functions/functions-2.js
+++
@@ -1,9 +0,0 @@
-//​​ Write a function called gooseFilter that takes an array of strings as an argument
-// and returns a filtered array containing the same elements but with the 'geese' removed.
-
-const gooseFilter = (birds) => {
-  const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-  const birdResult = birds.filter(birdName => geese.includes(birdName))
-  console.log(birdResult);
-  return birdResult;
-};
