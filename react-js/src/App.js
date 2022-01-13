dba6ff786d7bbc78d90f09956036ad4cb8ba3b03
Marie20767 <marie20767@gmail.com>
2022-01-13 15:38:31 +0000

    Added new react component called MyComponent

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import MyComponent from './MyComponent';
 
 const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      <MyComponent />
     </div>
   );
 };
---
+++ react-js/src/MyComponent.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const MyComponent = () => {
+  const [buttonClickCount, setButtonClickCount] = useState(0);
+  const onPlusButtonClicked = () => {
+    console.log("Yeahhhh");
+    setButtonClickCount(buttonClickCount + 1);
+  };
+  const onMinusButtonClicked = () => {
+    console.log("Noooo");
+    setButtonClickCount(buttonClickCount - 1);
+  };
+  return (
+    <div>
+      <h3>My Component</h3>
+      <button type="button" onClick={onPlusButtonClicked}>
+        +
+      </button>
+      <button type="button" onClick={onMinusButtonClicked}>
+        -
+      </button>
+      <h4>
+        Click count:
+        {buttonClickCount}
+      </h4>
+    </div>
+  );
+};
+
+export default MyComponent;
