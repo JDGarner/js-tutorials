da4eb13c58b5eeb936d7cad5f833ab9cb1434f00
Jamie Garner <jamiegarner123@gmail.com>
2022-01-25 17:08:46 +0000

    add more react exercises

---
+++ react-js/src/exercises-1/part-13/PartThirteen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { yourCoolData } from './some-data';
+
+// TODO:
+// 1 - Create an array of objects containing whatever data you like, something fun
+
+// 2 - Render it out below using .map
+
+// 3 - Experiment using .map to modify the data in some way before it gets rendered
+
+// If you're not sure what to do, look at the some documentation for different
+// methods that can be used to modify strings and numbers:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+// https://www.w3schools.com/js/js_arithmetic.asp
+
+// You can be creative and combine multiple methods together also if you want
+
+const PartThirteen = () => {
+  return (
+    <div>
+      <h4>Your cool data:</h4>
+    </div>
+  );
+};
+
+export default PartThirteen;
