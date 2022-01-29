0e45fe9c06af4df32d640cad16110bcf7dd24e68
Jamie Garner <jamiegarner123@gmail.com>
2022-01-29 12:26:24 +0000

    add more react exercises

--- react-js/src/exercises-1-unsolved/part-16/PartSixteen.js
+++ react-js/src/exercises-1-unsolved/part-16/PartSixteen.js
@@ -8,17 +8,28 @@ import React, { useState } from 'react';
 // 3 - When onClickFlipCoin is called, make it so there is a 50% chance that
 // coinFlipResult gets set to 'Heads' and a 50% chance that it gets set to 'Tails'
 
-// 4 - If the coinFlipResult is 'Heads', render an h4 in the component that says:
-// 'Wow it's heads!!!'
-// If it is 'Tails', render an h5 that says:
-// ':(( It's tails noooooooo'
+// 4 - Look at the code for rendering the h5 elements if coinFlipResult is heads or tails
+// See if you can figure out the syntax for this
+// This is using a 'ternary' if statement.
+// Read this if you're unsure on how the ternary if statement works:
+// https://www.programiz.com/javascript/ternary-operator
 
 const PartSixteen = () => {
   const [coinFlipResult, setCoinFlipResult] = useState('');
 
   return (
     <div>
-      {/* Your JSX here */}
+      {/* Your button here */}
+
+      {coinFlipResult === 'Heads'
+        ? <h5>Heads!!!</h5>
+        : null
+      }
+
+      {coinFlipResult === 'Tails'
+        ? <h5>Tails!!!</h5>
+        : null
+      }
     </div>
   );
 };
