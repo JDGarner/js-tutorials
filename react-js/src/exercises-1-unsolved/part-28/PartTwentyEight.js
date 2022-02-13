fc7fd9a3c3ebae236d4e608cbd54aa1d92310b98
Jamie Garner <jamiegarner123@gmail.com>
2022-02-13 12:53:58 +0200

    add another exercise

---
+++ react-js/src/exercises-1-unsolved/part-28/PartTwentyEight.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// TODO:
+// 1 - Add two text inputs, one will be used for the username
+// another for the password
+
+// 2 - Add two state variables using useState, one for the username, the other for password
+
+// 3 - Each time the values in the inputs change, update the appropriate state variable
+
+// 4 - Add a button with the text 'Sign In'
+
+// 5 - When the button is clicked, fire a function called onClickSignIn that
+// console.logs the values entered in the username, password fields
+
+// 6 - When this is working, check part-28-next-steps.txt for what to do next :)
+
+const PartTwentyEight = () => {
+  return (
+    <div>
+      <h4>Enter your username and password to sign in</h4>
+      {/* Add your inputs here */}
+    </div>
+  );
+};
+
+export default PartTwentyEight;
