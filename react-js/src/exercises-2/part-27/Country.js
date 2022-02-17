822044ba7c4f9679197fa1c54ee06b6cf73832d8
Marie20767 <marie20767@gmail.com>
2022-02-17 16:05:25 +0200

    Continued ex.27

--- react-js/src/exercises-2/part-27/Country.js
+++ react-js/src/exercises-2/part-27/Country.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
-const Country = ({ name, capital }) => {
+const Country = ({ name, capital, onClickSelectMyCountry, onClickAddCountryToFavourites, onClickRemoveFromFavourites }) => {
   return (
     <div>
       <h4>Name: {name}</h4>
       <h5>Capital: {capital}</h5>
+      {onClickSelectMyCountry
+        ? (
+          <button
+            type="button"
+            onClick={onClickSelectMyCountry}>Select country
+          </button>
+        )
+        : null
+      }
+      {onClickAddCountryToFavourites
+        ? (
+          <button type="button" onClick={onClickAddCountryToFavourites}>Add to favourites</button>
+        )
+        : null
+      }
+      {onClickRemoveFromFavourites
+        ? (
+          <button type="button" onClick={onClickRemoveFromFavourites}>Delete country</button>
+        )
+        : null
+    }
     </div>
   );
 };
--- react-js/src/exercises-2/part-27/PartTwentySeven.js
+++ react-js/src/exercises-2/part-27/PartTwentySeven.js
@@ -17,6 +17,9 @@ import Country from './Country';
 const PartTwentySeven = () => {
   const [countries, setCountries] = useState([]);
   const [showOnlyCountriesWithL, setShowOnlyCountriesWithL] = useState(false);
+  const [inputText, setInputText] = useState('');
+  const [myCountry, setMyCountry] = useState(null);
+  const [myFavouriteCountries, setMyFavouriteCountries] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:3000/countries')
@@ -29,6 +32,29 @@ const PartTwentySeven = () => {
       });
   }, []);
 
+  const onChangeInputText = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const onClickSelectMyCountry = (clickedOnCountry) => {
+    setMyCountry(clickedOnCountry);
+  };
+
+  const onClickAddCountryToFavourites = (clickedOnCountry) => {
+    const newFavouriteCountries = [...myFavouriteCountries, clickedOnCountry];
+
+    setMyFavouriteCountries(newFavouriteCountries);
+  };
+
+  const onClickRemoveFromFavourites = (clickedOnCountry) => {
+    // remove the clickedOnCountry that is passed into this function from the myFavouriteCountries array
+    const newFavouriteCountries = myFavouriteCountries.filter((favouritedCountry) => {
+      return clickedOnCountry.name !== favouritedCountry.name;
+    });
+
+    setMyFavouriteCountries(newFavouriteCountries);
+  };
+
   const onClickShowCountriesWithL = () => {
     setShowOnlyCountriesWithL(true);
   };
@@ -37,13 +63,51 @@ const PartTwentySeven = () => {
     setShowOnlyCountriesWithL(false);
   };
 
+  const isCountryInFavouriteCountries = (country) => {
+    return myFavouriteCountries.some((favouritedCountry) => {
+      return favouritedCountry.name === country.name;
+    });
+  };
+
   return (
     <div>
+      <h1>My Country</h1>
+      {myCountry
+        ? (
+          <Country
+            key={myCountry.name}
+            name={`${myCountry.name} ${myCountry.emoji}`}
+            capital={myCountry.capital} />
+        )
+        : null
+      }
+      <h1>My Favourite Countries</h1>
+      {myFavouriteCountries
+        ? myFavouriteCountries.map((country) => {
+          return (
+            <Country
+              key={country.name}
+              name={`${country.name} ${country.emoji}`}
+              capital={country.capital}
+              onClickRemoveFromFavourites={() => onClickRemoveFromFavourites(country)} />
+          );
+        })
+        : null
+    }
       <h1>Countries List</h1>
+      <input type="text" onChange={onChangeInputText} value={inputText} />
       <button type="button" onClick={onClickShowCountriesWithL}>Show countries with L</button>
       <button type="button" onClick={onClickShowAllCountries}>Show all countries</button>
       {countries.map((country) => {
-        if (!country.name.startsWith('L') && showOnlyCountriesWithL === true) {
+        if (isCountryInFavouriteCountries(country)) {
+          return null;
+        }
+
+        if (!country.name.toLowerCase().includes(inputText.toLowerCase())) {
+          return null;
+        }
+
+        if (!country.name.startsWith('L') && showOnlyCountriesWithL) {
           return null;
         }
 
@@ -51,7 +115,9 @@ const PartTwentySeven = () => {
           <Country
             key={country.name}
             name={`${country.name} ${country.emoji}`}
-            capital={country.capital} />
+            capital={country.capital}
+            onClickSelectMyCountry={() => onClickSelectMyCountry(country)}
+            onClickAddCountryToFavourites={() => onClickAddCountryToFavourites(country)} />
         );
       })}
     </div>
