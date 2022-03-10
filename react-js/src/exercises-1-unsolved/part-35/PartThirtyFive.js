4a7a261596c2e065f8f91bd6711ce0355820978d
Jamie Garner <jamiegarner123@gmail.com>
2022-03-10 13:59:51 +0000

    part 35

---
+++ react-js/src/exercises-1-unsolved/part-35/PartThirtyFive.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// TODO: - read introduction
+// The idea for this app is to make another German/English language learning game :).
+// But a bit different from the previous one
+
+// We want to have something like:
+// - 4 buttons show up on one side, each a random German word in them
+// - 4 buttons show up next to these, each with an English translation
+//   of one of the German words in the first set of buttons
+
+// The object of the game is to match the German words with the correct
+// English translation
+
+// TODO: - lets discuss how we want the game to work exactly before starting
+
+// TODO: first steps:
+// 1 - Look at data.js, import the wordData array from it into this file
+
+// 2 - Create a state variable called currentWords
+
+// 3 - We need to set the initial values of currentWords to
+// an array of 4 random entries from the wordData array. Look in utils.js,
+// complete the function in there and then use that to set the initial value of
+// the currentWords variable, e.g:
+// const [currentWords, setCurrentWords] = useState(generatedFourRandomWords(wordData));
+
+const PartThirtyFive = () => {
+  return (
+    <div>Hello</div>
+  );
+};
+
+export default PartThirtyFive;
---
+++ react-js/src/exercises-1/part-35/utils.js
@@ -0,0 +1,7 @@
+const generatedFourRandomWords = (wordData) => {
+  // should return an array of four random objects in the wordData array
+
+  // If you're not sure, think about how you get one random entry from the wordData
+  // array first.
+  // Then how would you put that random entry into a new array.
+};
